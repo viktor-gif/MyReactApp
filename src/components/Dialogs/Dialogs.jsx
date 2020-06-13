ee4c9693fbba74108d11b784aa1fbb0b1f0d5995
Viktor <klim.victor.ua@gmail.com>
2020-06-14 01:23:30 +0300

    lesson 24

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,43 +2,60 @@ import React from "react";
 import s from "./Dialogs.module.css";
 import { NavLink } from "react-router-dom";
 
+const DialogItem = (props) => {
+  let path = "/dialogs/" + props.id;
+
+  return (
+    <div className={s.dialog + " " + s.active}>
+      <NavLink to={path}>{props.name}</NavLink>
+    </div>
+  );
+};
+
+const Message = (props) => {
+  let path = "/dialogs/" + props.id;
+
+  return <div className={s.dialog}>{props.message}</div>;
+};
+
 const Dialogs = (props) => {
+
+  let dialogsData = [
+    {id: 1, name: 'Klim'},
+    {id: 2, name: 'Vorobey'},
+    {id: 3, name: 'Kit Semen'},
+    {id: 4, name: 'Shumak'},
+    {id: 5, name: 'Igoriok'},
+    {id: 6, name: 'Yarik'},
+  ]
+
+  let messagesData = [
+    {id: 1, message: 'Hi!'},
+    {id: 2, message: 'How are you?'},
+    {id: 3, message: 'Everything is cool!'},
+    {id: 4, message: 'Yo'},
+    {id: 5, message: 'Yo'},
+    
+  ]
+
   return (
     <div className={s.dialogs}>
-
-      <div className={s.ialogs__items}>
-          <div className={s.dialog + ' ' + s.active}>
-            <NavLink to="/dialogs/1">Klim</NavLink>
-          </div>
-          <div className={s.dialog}>
-          <NavLink to="/dialogs/2">Vorobey</NavLink>
-          </div>
-          <div className={s.dialog}>
-          <NavLink to="/dialogs/3">Shumak</NavLink>
-          </div>
-          <div className={s.dialog}>
-          <NavLink to="/dialogs/4">Kit Semen</NavLink>
-          </div>
-          <div className={s.dialog}>
-          <NavLink to="/dialogs/5">Igoriok</NavLink>
-          </div>
-          <div className={s.dialog}>
-          <NavLink to="/dialogs/6">Yarik</NavLink>
-          </div>
+      <div className={s.dialogs__items}>
+        <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+        <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
+        <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
+        <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
+        <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />
+        <DialogItem name={dialogsData[5].name} id={dialogsData[5].id} />
       </div>
 
-        <div className={s.messages}>
-          <div className={s.message}>
-            Hello!
-          </div>
-          <div className={s.message}>
-            How are you?
-          </div>
-          <div className={s.message}>
-            Everything is cool!
-          </div>
+      <div className={s.messages}>
+        <Message message={messagesData[0].message} />
+        <Message message={messagesData[1].message} />
+        <Message message={messagesData[2].message} />
+        <Message message={messagesData[3].message} />
+        <Message message={messagesData[4].message} />
       </div>
-      
     </div>
   );
 };
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,23 +1,32 @@
-import React from 'react';
-import s from './MyPosts.module.css';
-import Post from './Post/Post';
+import React from "react";
+import s from "./MyPosts.module.css";
+import Post from "./Post/Post";
 
 const MyPosts = () => {
+  
+  let postData = [
+    {id: 1, message: 'Hi, hw are you?', likesCount: 56},
+    {id: 2, message: 'It\'s my first post', likesCount: 76},
+    {id: 3, message: 'It\'s my second post', likesCount: 45}
+    
+  ]
+
   return (
-    <div>
-      My posts
+    <div className={s.MyPostsWrap}>
+      <h3>My posts</h3>
       <div>
         <textarea></textarea>
+      </div>
+      <div className={s.buttonAddPost}>
         <button>Add post</button>
-
       </div>
       <div className={s.posts}>
-        <Post message='Hi, how are you?' likesCount='45' />
-        <Post message="It\'s my first post" likesCount='26' />
+        <Post className={s.post} message={postData[0].message} likesCount={postData[0].likesCount} />
+        <Post className={s.post} message={postData[1].message} likesCount={postData[1].likesCount} />
+        <Post className={s.post} message={postData[2].message} likesCount={postData[2].likesCount} />
       </div>
     </div>
-  )
-
-}
+  );
+};
 
 export default MyPosts;
