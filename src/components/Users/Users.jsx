765b78256d72a4b94670fa050e938145c8d06b51
Viktor <klim.victor.ua@gmail.com>
2020-08-07 00:33:10 +0300

    lesson 51

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,36 +1,44 @@
 import React from "react";
 import s from "./Users.module.css";
+import * as axios from "axios";
+import userPhoto from "../../assets/images/user.jpg"
 
 const Users = (props) => {
 
   if (props.users.length === 0) {
-    props.setUsers([
-            {
-                id: 1,
-                photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/220px-Dmitry_Nagiev_2017_4.jpg',
-                followed: false,
-                fullName: 'Dmitry',
-                status: 'I am a boss',
-                location: {city: 'Minsk', country: 'Belarus'}
-            },
-            {
-                id: 2,
-                photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/220px-Dmitry_Nagiev_2017_4.jpg',
-                followed: true,
-                fullName: 'Sasha',
-                status: 'I am a boss too',
-                location: {city: 'Moscow', country: 'Russia'}
-            },
-            {
-                id: 3,
-                photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/220px-Dmitry_Nagiev_2017_4.jpg',
-                followed: false,
-                fullName: 'Andrew',
-                status: 'I am a boss too',
-                location: {city: 'Kiev', country: 'Ukraine'}
-            }
-        ]
-    )
+
+    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+      
+      props.setUsers(response.data.items);
+    });
+
+//     props.setUsers([
+//             {
+//                 id: 1,
+//                 photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/220px-Dmitry_Nagiev_2017_4.jpg',
+//                 followed: false,
+//                 fullName: 'Dmitry',
+//                 status: 'I am a boss',
+//                 location: {city: 'Minsk', country: 'Belarus'}
+//             },
+//             {
+//                 id: 2,
+//                 photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/220px-Dmitry_Nagiev_2017_4.jpg',
+//                 followed: true,
+//                 fullName: 'Sasha',
+//                 status: 'I am a boss too',
+//                 location: {city: 'Moscow', country: 'Russia'}
+//             },
+//             {
+//                 id: 3,
+//                 photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/220px-Dmitry_Nagiev_2017_4.jpg',
+//                 followed: false,
+//                 fullName: 'Andrew',
+//                 status: 'I am a boss too',
+//                 location: {city: 'Kiev', country: 'Ukraine'}
+//             }
+//         ]
+//     )
 }
 
   return <div>
@@ -38,7 +46,7 @@ const Users = (props) => {
       props.users.map(u => <div key={u.id}>
           <span>
             <div>
-              <img src={u.photoUrl} className={s.userPhoto} />
+              <img src={u.photos.small != null ? u.photos.small: userPhoto} className={s.userPhoto} />
             </div>
             <div>
               {u.followed 
@@ -53,12 +61,12 @@ const Users = (props) => {
           </span>
           <span>
             <span>
-              <div>{u.fullName}</div>
+              <div>{u.name}</div>
               <div>{u.status}</div>
             </span>
             <span>
-              <div>{u.location.city}</div>
-              <div>{u.location.country}</div>
+              <div>{"u.location.city"}</div>
+              <div>{"u.location.country"}</div>
             </span>
           </span>
         </div>)
