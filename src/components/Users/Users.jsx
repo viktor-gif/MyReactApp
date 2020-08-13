5eef4f4e36ddd85d9133dac28e5efdfa93bcc8ac
Viktor <klim.victor.ua@gmail.com>
2020-08-13 06:12:14 +0300

    lesson 53

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,47 +3,21 @@ import s from "./Users.module.css";
 import * as axios from "axios";
 import userPhoto from "../../assets/images/user.jpg"
 
-const Users = (props) => {
+class Users extends React.Component {
 
-  if (props.users.length === 0) {
+  constructor(props) {
+    super(props);
+      axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+        this.props.setUsers(response.data.items);
+      });
+  }
 
-    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-      
-      props.setUsers(response.data.items);
-    });
+  
 
-//     props.setUsers([
-//             {
-//                 id: 1,
-//                 photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/220px-Dmitry_Nagiev_2017_4.jpg',
-//                 followed: false,
-//                 fullName: 'Dmitry',
-//                 status: 'I am a boss',
-//                 location: {city: 'Minsk', country: 'Belarus'}
-//             },
-//             {
-//                 id: 2,
-//                 photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/220px-Dmitry_Nagiev_2017_4.jpg',
-//                 followed: true,
-//                 fullName: 'Sasha',
-//                 status: 'I am a boss too',
-//                 location: {city: 'Moscow', country: 'Russia'}
-//             },
-//             {
-//                 id: 3,
-//                 photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/220px-Dmitry_Nagiev_2017_4.jpg',
-//                 followed: false,
-//                 fullName: 'Andrew',
-//                 status: 'I am a boss too',
-//                 location: {city: 'Kiev', country: 'Ukraine'}
-//             }
-//         ]
-//     )
-}
-
-  return <div>
+  render() {
+    return <div>
       {
-      props.users.map(u => <div key={u.id}>
+      this.props.users.map(u => <div key={u.id}>
           <span>
             <div>
               <img src={u.photos.small != null ? u.photos.small: userPhoto} className={s.userPhoto} />
@@ -51,10 +25,10 @@ const Users = (props) => {
             <div>
               {u.followed 
               ? <button onClick={() => {
-                  props.unfollow(u.id)
+                this.props.unfollow(u.id)
               }}>Unfollow</button>
               : <button onClick={() => {
-                props.follow(u.id)
+                this.props.follow(u.id)
               }}>Follow</button>}
                     
             </div>
@@ -72,6 +46,7 @@ const Users = (props) => {
         </div>)
       }
     </div>
-    }
+  }
+}
 
 export default Users;    
