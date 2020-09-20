7c24e25f8fdf249a907ddef6e6890def8e814d3c
viktor <klim-victor.ua@gmail.com>
2020-09-20 16:21:53 +0300

    lesson 88

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,38 +1,54 @@
-import React from "react";
+import React, { Component, PureComponent } from "react";
 import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 import { Field, reduxForm } from "redux-form";
-import { required, maxLengthCreator } from "../../../utils/validators/validators";
-import {Textarea} from "../../common/FormsControl/FormsControl";
+import {
+  required,
+  maxLengthCreator,
+} from "../../../utils/validators/validators";
+import { Textarea } from "../../common/FormsControl/FormsControl";
 
 const maxLength10 = maxLengthCreator(10);
 
 const AddNewPostForm = (props) => {
-
-  return <form onSubmit={props.handleSubmit}>
-    <div>
-    <Field name="newPostText" component={Textarea} placeholder={"Post message"}
-                   validate={[required, maxLength10]} />
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <div>
+        <Field
+          name="newPostText"
+          component={Textarea}
+          placeholder={"Post message"}
+          validate={[required, maxLength10]}
+        />
       </div>
       <div className={s.buttonAddPost}>
         <button>Add post</button>
       </div>
-  </form>
-  
-}
+    </form>
+  );
+};
 
-const AddNewPostFormRedux = reduxForm({form: 'profileAddNewPostForm'})(AddNewPostForm)
+const AddNewPostFormRedux = reduxForm({ form: "profileAddNewPostForm" })(
+  AddNewPostForm
+);
 
-
-const MyPosts = (props) => {
-  let postsElements = props.posts.map((p) => (
-    <Post message={p.message} key={p.id} id={p.id} likesCount={p.likesCount} />
-  ));
+const MyPosts = React.memo((props) => {
+  console.log("RENDER");
+  let postsElements = [...props.posts]
+    .reverse()
+    .map((p) => (
+      <Post
+        message={p.message}
+        key={p.id}
+        id={p.id}
+        likesCount={p.likesCount}
+      />
+    ));
 
   const onAddPost = (values) => {
     props.addPost(values.newPostText);
-  }
-  
+  };
+
   return (
     <div className={s.MyPostsWrap}>
       <h3>My posts</h3>
@@ -40,6 +56,6 @@ const MyPosts = (props) => {
       <div className={s.posts}>{postsElements}</div>
     </div>
   );
-};
+});
 
 export default MyPosts;
