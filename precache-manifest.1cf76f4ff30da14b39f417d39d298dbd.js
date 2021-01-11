2c89620c750480ba741fe7988cb046acc77b3196
viktor <klim-victor.ua@gmail.com>
2021-01-11 16:00:37 +0200

    Updates

---
+++ precache-manifest.1cf76f4ff30da14b39f417d39d298dbd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29a4b7532188adf8691504d41d6b5191",
+    "url": "/MyReactApp/index.html"
+  },
+  {
+    "revision": "4abb645d8ea2c0889e9c",
+    "url": "/MyReactApp/static/css/3.94a2caa6.chunk.css"
+  },
+  {
+    "revision": "de3715aa2c6c4c8dbec1",
+    "url": "/MyReactApp/static/css/4.f04ad11d.chunk.css"
+  },
+  {
+    "revision": "46eaa669f369ff567d06",
+    "url": "/MyReactApp/static/css/main.119c895d.chunk.css"
+  },
+  {
+    "revision": "5cb4bbcc4ae39d746510",
+    "url": "/MyReactApp/static/js/2.80ccb538.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MyReactApp/static/js/2.80ccb538.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4abb645d8ea2c0889e9c",
+    "url": "/MyReactApp/static/js/3.709ca7a7.chunk.js"
+  },
+  {
+    "revision": "de3715aa2c6c4c8dbec1",
+    "url": "/MyReactApp/static/js/4.685eb69b.chunk.js"
+  },
+  {
+    "revision": "46eaa669f369ff567d06",
+    "url": "/MyReactApp/static/js/main.81c37d9c.chunk.js"
+  },
+  {
+    "revision": "3adcf5ebc3a8e9144d8d",
+    "url": "/MyReactApp/static/js/runtime-main.a4c29789.js"
+  }
+]);
